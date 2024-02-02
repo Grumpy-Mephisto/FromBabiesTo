baf4b2afdfcd2d891cb9c7cb9cae378feff539bd
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-02-03 00:58:30 +0700

    🧹 chore(animation): prepare for da the best thing

--- AnimationPanel.java
+++ AnimationPanel.java
@@ -16,11 +16,6 @@ public class AnimationPanel extends JPanel {
     private int characterAge = 0;
     private Point characterPosition; // Character position
     private Point characterVelocity; // Movement speed
-    private LifeStage currentStage = LifeStage.BABY;
-
-    private enum LifeStage {
-        BABY, CHILD, TEENAGER, ADULT, ELDER
-    }
 
     public AnimationPanel() {
         initializeCanvas();
@@ -48,7 +43,6 @@ public class AnimationPanel extends JPanel {
     private void updateCharacter() {
         characterAge = (characterAge + 1) % MAX_AGE;
 
-        updateLifeStage();
         updatePosition();
 
         bufferGraphics.setComposite(AlphaComposite.Clear);
@@ -57,20 +51,6 @@ public class AnimationPanel extends JPanel {
 
         drawBackground(bufferGraphics);
         drawCharacter(bufferGraphics);
-    }
-
-    private void updateLifeStage() {
-        if (characterAge <= 20) {
-            currentStage = LifeStage.BABY;
-        } else if (characterAge <= 40) {
-            currentStage = LifeStage.CHILD;
-        } else if (characterAge <= 60) {
-            currentStage = LifeStage.TEENAGER;
-        } else if (characterAge <= 80) {
-            currentStage = LifeStage.ADULT;
-        } else {
-            currentStage = LifeStage.ELDER;
-        }
     }
 
     private void updatePosition() {
@@ -98,101 +78,88 @@ public class AnimationPanel extends JPanel {
     }
 
     private void drawBackground(Graphics2D g2d) {
-        g2d.setColor(getBackgroundColor());
+        g2d.setColor(Color.WHITE);
         g2d.fillRect(0, 0, getWidth(), getHeight());
     }
 
-    private Color getBackgroundColor() {
-        switch (currentStage) {
-            case BABY:
-                return Color.PINK;
-            case CHILD:
-                return Color.CYAN;
-            case TEENAGER:
-                return Color.ORANGE;
-            case ADULT:
-                return Color.GREEN;
-            case ELDER:
-                return Color.GRAY;
-            default:
-                return Color.WHITE;
-        }
-    }
-
     private void drawCharacter(Graphics2D g2d) {
-        int size = 40 + (characterAge / 5); // Character size changes with age
-        int earSize = size / 4;
-
-        // Drawing the body of the cat using Midpoint Circle
-        Color characterColor = getStageColor();
-        drawMidpointCircle(g2d, characterPosition.x, characterPosition.y, size / 2);
-        floodFill(buffer, characterPosition.x, characterPosition.y, new Color(0, 0, 0, 0),
-                characterColor);
-
-        // Drawing ears with Bézier curves
-        drawBezierEar(g2d, characterPosition.x - size / 2, characterPosition.y - size / 2, earSize);
-        drawBezierEar(g2d, characterPosition.x + size / 2, characterPosition.y - size / 2, earSize);
-
-        // Drawing eyes
-        g2d.setColor(Color.BLACK);
-        g2d.fillOval(characterPosition.x - size / 4, characterPosition.y - size / 4, size / 10,
-                size / 10);
-        g2d.fillOval(characterPosition.x + size / 4 - size / 10, characterPosition.y - size / 4,
-                size / 10, size / 10);
-
-        // Drawing tail with Bézier curve
-        drawBezierTail(g2d, characterPosition.x + size / 2, characterPosition.y, size);
+        tRexRun(g2d);
     }
+
+    private void tRexRun(Graphics2D g2d) {
+        g2d.setColor(Color.RED);
+
+        int size = 40 + (characterAge / 5);
+
+        // Draw the body
+
+    }
+
 
     private void floodFill(BufferedImage image, int x, int y, Color targetColor,
             Color replacementColor) {
         int width = image.getWidth();
         int height = image.getHeight();
-        if (x < 0 || x >= width || y < 0 || y >= height) {
+        if (x < 0 || x >= width || y < 0 || y >= height)
             return;
-        }
-        if (!new Color(image.getRGB(x, y), true).equals(targetColor)) {
+
+        int targetRGB = targetColor.getRGB();
+        if (image.getRGB(x, y) != targetRGB)
             return;
-        }
 
         Stack<Point> stack = new Stack<>();
         stack.push(new Point(x, y));
 
-        while (!stack.empty()) {
-            Point point = stack.pop();
-            x = point.x;
-            y = point.y;
+        while (!stack.isEmpty()) {
+            Point p = stack.pop();
+            int px = p.x;
+            int py = p.y;
 
-            if (x < 0 || x >= width || y < 0 || y >= height) {
+            if (px < 0 || px >= width || py < 0 || py >= height)
                 continue;
+            if (image.getRGB(px, py) != targetRGB)
+                continue;
+
+            image.setRGB(px, py, replacementColor.getRGB());
+
+            stack.push(new Point(px + 1, py));
+            stack.push(new Point(px - 1, py));
+            stack.push(new Point(px, py + 1));
+            stack.push(new Point(px, py - 1));
+        }
+    }
+
+
+    private void drawBresenhamLine(Graphics2D g2d, int x1, int y1, int x2, int y2) {
+        int dx = Math.abs(x2 - x1);
+        int dy = Math.abs(y2 - y1);
+
+        int sx = x1 < x2 ? 1 : -1;
+        int sy = y1 < y2 ? 1 : -1;
+
+        int err = dx - dy;
+        int e2;
+
+        while (true) {
+            g2d.fillRect(x1, y1, 1, 1); // Draw the pixel at (x1, y1)
+
+            if (x1 == x2 && y1 == y2) {
+                break;
             }
 
-            if (new Color(image.getRGB(x, y), true).equals(targetColor)) {
-                image.setRGB(x, y, replacementColor.getRGB());
+            e2 = 2 * err;
+            if (e2 > -dy) {
+                err -= dy;
+                x1 += sx;
+            }
 
-                stack.push(new Point(x + 1, y));
-                stack.push(new Point(x - 1, y));
-                stack.push(new Point(x, y + 1));
-                stack.push(new Point(x, y - 1));
+            if (e2 < dx) {
+                err += dx;
+                y1 += sy;
             }
         }
     }
 
-    private void drawBezierEar(Graphics2D g2d, int x, int y, int size) {
-        Point p1 = new Point(x, y);
-        Point p2 = new Point(x + size / 2, y - size);
-        Point p3 = new Point(x + size, y);
-
-        drawBezierCurve(g2d, p1, p2, p3);
-    }
-
-    private void drawBezierTail(Graphics2D g2d, int x, int y, int length) {
-        Point p1 = new Point(x, y);
-        Point p2 = new Point(x + length / 2, y + length / 2);
-        Point p3 = new Point(x + length, y);
-
-        drawBezierCurve(g2d, p1, p2, p3);
-    }
 
     private void drawBezierCurve(Graphics2D g2d, Point p1, Point p2, Point p3) {
         Path2D path = new Path2D.Double();
@@ -235,22 +202,5 @@ public class AnimationPanel extends JPanel {
         g2d.fillRect(xCenter - y, yCenter + x, 1, 1);
         g2d.fillRect(xCenter + y, yCenter - x, 1, 1);
         g2d.fillRect(xCenter - y, yCenter - x, 1, 1);
-    }
-
-    private Color getStageColor() {
-        switch (currentStage) {
-            case BABY:
-                return Color.YELLOW;
-            case CHILD:
-                return Color.BLUE;
-            case TEENAGER:
-                return Color.MAGENTA;
-            case ADULT:
-                return Color.RED;
-            case ELDER:
-                return Color.DARK_GRAY;
-            default:
-                return Color.BLACK;
-        }
     }
 }
